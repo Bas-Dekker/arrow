d584a5f18104f5da89c39c3912a37d2b16208e02
Bas Dekker <bmjdekker@gmail.com>
2021-03-26 13:48:12 +0100

    Arrow Functions opdracht toegevoegd

---
+++ script.js
@@ -0,0 +1,26 @@
+/* A. orginineel:
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+*/
+
+const ikRockArrowFunctions = () => {
+  console.log("Joe, ik rock arrow functions!");
+};
+
+/* B. origineel:
+const fivePlusSeven = function () {
+  return 5 + 7
+};
+*/
+
+const fivePlusSeven = () => 5 + 7;
+
+/* C:  */
+const myFunction = (a, b) => a + b;
+
+/* D:   */
+const addFive = (a) => a + 5;
+
+/* E:    */
+const createObject = () => console.log({ greeting: "hoi" });
